8c2541cc54dbb9679eeb7c1d67abc5262c6b112e
chelsea427 <ahnjh1997@naver.com>
2026-03-27 09:43:43 +0900

    flask migrate

---
+++ 1차 Project(Flask,머신러닝)/flask/migrations/versions/eee76cb7fd37_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: eee76cb7fd37
+Revises: b776ca5baf4c
+Create Date: 2026-03-27 09:40:34.643814
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import oracle
+
+# revision identifiers, used by Alembic.
+revision = 'eee76cb7fd37'
+down_revision = 'b776ca5baf4c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
