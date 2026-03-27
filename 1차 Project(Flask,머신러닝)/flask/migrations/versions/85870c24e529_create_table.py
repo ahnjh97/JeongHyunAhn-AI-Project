f141a9ff0da9c53aaaaf05f65ca86aa9d7e1956c
chelsea427 <ahnjh1997@naver.com>
2026-03-27 10:21:46 +0900

    migration 초기화

---
+++ 1차 Project(Flask,머신러닝)/flask/migrations/versions/85870c24e529_create_table.py
@@ -0,0 +1,24 @@
+"""create table
+
+Revision ID: 85870c24e529
+Revises: 
+Create Date: 2026-03-27 10:16:32.682627
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import oracle
+
+# revision identifiers, used by Alembic.
+revision = '85870c24e529'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
